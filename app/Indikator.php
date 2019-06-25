f13647e21204d0bd7c385e8690861256b8941ef6
Albertus Tjahjono <fleksaboy@gmail.com>
2019-06-25 09:00:28 +0700

    .

--- app/Indikator.php
+++ app/Indikator.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Indikator extends Model
 {
-    //
+    protected $fillable = [
+        'indikator', 'n-2', 'n', 'n+1', 'n+2', 'n+3', "kondisi_akhir"
+    ];
+
+
+    public function tujuan()
+    {
+        return $this->belongsTo('App\Tujuan');
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function bobotsasaran()
+    {
+        return $this->hasMany('App\BobotSasaran');
+    }
+    public function bobotkriteriamisi()
+    {
+        return $this->hasMany('App\BobotSasaran', 'sasaran_id', 'id')->where('user_id', Auth::user()->id);
+    }
+    public function bobotkriteriamisi2()
+    {
+        return $this->hasMany('App\BobotSasaran', 'sasaran2_id', 'id')->where('user_id', Auth::user()->id);
+    }
+    public function eigenmisi()
+    {
+        return $this->hasMany('App\EigenSasaran', 'sasaran_id', 'id');
+    }
 }
